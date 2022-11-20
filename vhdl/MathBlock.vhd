4dacff29f605519fb84fe450c60c051905412cb8
Benjamin Bean <gladclef@users.noreply.github.com>
2022-11-20 10:56:09 -0700

    working on rendering the math block, part 2

--- vhdl/MathBlock.vhd
+++ vhdl/MathBlock.vhd
@@ -18,7 +18,7 @@
 --
 -----------------------------------------------------------
 -- FSM created with https://github.com/gladclef/FSMs
--- {'fsm_name':'MathBlock','table_vals':[['', 'reset', 'start', '__', '___', 'text_ready', 'stop', 'off_screen'], ['IDLE', '', 'LOAD', '', '', '', '', ''], ['LOAD', '', '', 'ASCII_START', '', '', '', ''], ['ASCII_START', '', '', '', 'ASCII_WAIT', '', '', ''], ['ASCII_WAIT', '', '', '', '', 'RENDER', '', ''], ['RENDER', '', '', '', '', '', 'IDLE', 'IDLE']]}
+-- {'fsm_name': 'MathBlock', 'table_vals': [['', 'reset', 'start', '__', '___', 'text_ready', 'v_back_porch', 'v_front_port', 'stop', 'off_screen'], ['IDLE', '', 'LOAD', '', '', '', '', '', '', ''], ['LOAD', '', '', 'ASCII_START', '', '', '', '', '', ''], ['ASCII_START', '', '', '', 'ASCII_WAIT', '', '', '', '', ''], ['ASCII_WAIT', '', '', '', '', 'RENDER', '', '', '', ''], ['RENDER', '', '', '', '', '', 'INTER_FRAME', '', 'IDLE', 'IDLE'], ['INTER_FRAME', '', '', '', '', '', '', 'RENDER', 'IDLE', 'IDLE']]}
 -----------------------------------------------------------
 
 library IEEE;
@@ -30,31 +30,37 @@ use work.DataTypes_pkg.all;
 
 entity MathBlock is
    Port (
-      reset : in std_logic;
-      clk : in std_logic;
-      start: in std_logic;
-      ready: out std_logic;
-      x: in std_logic_vector(10 downto 0);
-      ascii: in std_logic_vector(MATH_BLOCK_MAX_CHARS*ASCII_NB-1 downto 0);
+      reset:       in std_logic;
+      clk:         in std_logic;
+      start:       in std_logic;
+      ready:       out std_logic;
+      x:           in std_logic_vector(10 downto 0);
+      ascii:       in std_logic_vector(MATH_BLOCK_MAX_CHARS*ASCII_NB-1 downto 0);
       y_increment: in std_logic;
-      stop: in std_logic;
-      pix_x: in std_logic_vector(10 downto 0);
-      pix_y: in std_logic_vector(9 downto 0);
-      pix_en: out std_logic
+      stop:        in std_logic;
+      pix_x:       in std_logic_vector(10 downto 0);
+      pix_y:       in std_logic_vector(9 downto 0);
+      pix_en:      out std_logic
    );
 end MathBlock;
 
 architecture rtl of MathBlock is
-   type state_type is (IDLE, LOAD, ASCII_START, ASCII_WAIT, RENDER);
    type characters is array(0 to MATH_BLOCK_MAX_CHARS-1) of ascii_char;
-   
+   type state_type is (IDLE, LOAD, ASCII_START, ASCII_WAIT, RENDER, INTER_FRAME);
+
+   constant V_BACK_PORCH_VEC:  std_logic_vector(9 downto 0) := std_logic_vector(to_unsigned(480,10));
+   constant V_FRONT_PORCH_VEC: std_logic_vector(9 downto 0) := std_logic_vector(to_unsigned(481,10));
+
    signal state_reg, state_next: state_type;
    signal block_x_reg, block_x_next: integer range 0 to 1023;
    signal block_y_reg, block_y_next: integer range 0 to 511;
    signal ascii_reg, ascii_next: characters;
    -- TODO include render_nextx and render_nexty registers
    -- TODO include render_bits registers
+   signal text_start: std_logic;
    signal text_ready: std_logic;
+   signal text_pixels: std_logic_vector(MATH_BLOCK_MAX_CHARS*ASCII_NB-1 downto 0);
+   signal v_back_porch, v_front_porch: std_logic;
    signal off_screen: std_logic;
 
 begin
@@ -75,16 +81,19 @@ begin
       end if;
    end process;
 
+   v_back_porch <= '1' when (pix_y = V_BACK_PORCH_VEC) else '0';
+   v_front_porch <= '1' when (pix_y = V_FRONT_PORCH_VEC) else '0';
    off_screen <= '1' when (block_y_reg > 479) else '0';
    ready <= '1' when state_reg = IDLE else '0';
 
    -- combinational circuit
-   process(state_reg, reset, start, text_ready, stop, off_screen)
+   process(state_reg, reset, start, text_ready, stop, off_screen, v_back_porch, v_front_porch)
    begin
       state_next <= state_reg;
       block_x_next <= block_x_reg;
       block_y_next <= block_y_reg;
       ascii_next <= ascii_reg;
+      text_start <= '0';
 
       case state_reg is
          when IDLE =>
@@ -99,16 +108,31 @@ begin
             state_next <= ASCII_START;
 
          when ASCII_START =>
+            -- give the RenderText component a clock cycle to get started
             text_start <= '1';
             state_next <= ASCII_WAIT;
 
          when ASCII_WAIT =>
+            -- busy wait for the RenderText component to finish generating its bits
             if (text_ready = '1') then
                state_next <= RENDER;
             end if;
 
          when RENDER =>
-            if (stop = '1') then
+            -- render out the block bits as they come up
+            if (v_back_porch = '1') then
+               state_next <= INTER_FRAME;
+            elsif (stop = '1') then
+               state_next <= IDLE;
+            elsif (off_screen = '1') then
+               state_next <= IDLE;
+            end if;
+
+         when INTER_FRAME =>
+            -- state logic
+            if (v_front_port = '1') then
+               state_next <= RENDER;
+            elsif (stop = '1') then
                state_next <= IDLE;
             elsif (off_screen = '1') then
                state_next <= IDLE;
@@ -116,4 +140,14 @@ begin
 
       end case;
    end process;
+
+   render_text: entity work.RenderText(rtl)
+   port map (
+      reset  => reset,
+      clk    => clk,
+      start  => text_start,
+      ascii  => ascii_reg,
+      pixels => text_pixels,
+      ready  => text_ready
+   );
 end rtl;
--- vhdl/RenderText.vhd
+++ vhdl/RenderText.vhd
@@ -8,7 +8,7 @@
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
--- Description:    Renders the given characters as a matrix of 0's and 1's, to be interpretted as pixels.
+-- Description:    Renders the given characters as a matrix of 0's and 1's, to be interpreted as pixels.
 --
 -- Dependencies: 
 --
@@ -17,7 +17,7 @@
 -- Additional Comments: 
 -----------------------------------------------------------
 -- FSM created with https://github.com/gladclef/FSMs
--- {'fsm_name': 'RenderText', 'table_vals': [['', 'reset', 'start', 'has_char', '__'], ['IDLE', '', 'COUNT', '', ''], ['COUNT', '', '', 'RENDER', ''], ['RENDER', '', '', 'RENDER_CHAR', 'IDLE'], ['RENDER_CHAR', '', '', '', 'RENDER']]}
+-- {"fsm_name":"RenderText","table_vals":[["","reset","start","has_char","__"],["IDLE","","RENDER","",""],["RENDER","","","RENDER_CHAR","IDLE"],["RENDER_CHAR","","","","RENDER"]]}
 -----------------------------------------------------------
 
 library IEEE;
@@ -39,7 +39,7 @@ entity RenderText is
 end RenderText;
 
 architecture rtl of RenderText is
-   type state_type is (IDLE, COUNT, RENDER, RENDER_CHAR);
+   type state_type is (IDLE, RENDER, RENDER_CHAR);
    CONSTANT char_0 : std_logic_vector(0 to 14) := "111101101101111";
    CONSTANT char_1 : std_logic_vector(0 to 14) := "010010010010010";
    CONSTANT char_2 : std_logic_vector(0 to 14) := "111001111100111";
@@ -50,14 +50,23 @@ architecture rtl of RenderText is
    CONSTANT char_7 : std_logic_vector(0 to 14) := "111001001001001";
    CONSTANT char_8 : std_logic_vector(0 to 14) := "111101111101111";
    CONSTANT char_9 : std_logic_vector(0 to 14) := "111101111001001";
-   CONSTANT char_equ : std_logic_vector(0 to 14) := "000011000011000";
+   CONSTANT char_mul : std_logic_vector(0 to 14) := "000101010101000";
    CONSTANT char_plu : std_logic_vector(0 to 14) := "000010111010000";
    CONSTANT char_min : std_logic_vector(0 to 14) := "000000011000000";
    CONSTANT char_div : std_logic_vector(0 to 14) := "001001010100100";
-   CONSTANT char_mul : std_logic_vector(0 to 14) := "000101010101000";
+   CONSTANT char_equ : std_logic_vector(0 to 14) := "000011000011000";
+
+   CONSTANT row0 : integer := TEXT_BLOCK_WIDTH*0;
+   CONSTANT row1 : integer := TEXT_BLOCK_WIDTH*1;
+   CONSTANT row2 : integer := TEXT_BLOCK_WIDTH*2;
+   CONSTANT row3 : integer := TEXT_BLOCK_WIDTH*3;
+   CONSTANT row4 : integer := TEXT_BLOCK_WIDTH*4;
 
    signal state_reg, state_next: state_type;
-   signal has_char: std_logic;
+   signal idx_reg, idx_next: integer range 0 to MATH_BLOCK_MAX_CHARS;
+   signal count_reg, count_next: integer range 0 to MATH_BLOCK_MAX_CHARS-1;
+   signal render_x_reg, render_x_next: integer range 0 to TEXT_BLOCK_WIDTH+4;
+   signal pixels_reg, pixels_next: std_logic_vector(0 to TEXT_BLOCK_ADDR-1);
 begin
 
    -- state and data register
@@ -71,35 +80,84 @@ begin
    end process;
 
    -- combinational circuit
-   process(state_reg, reset, start, has_char, __)
+   process(state_reg, reset, start)
+      variable pix_start : integer;
+      variable ascii_val : std_logic_vector(ASCII_NB-1 downto 0);
+      variable char_bits : std_logic_vector(0 to 14);
    begin
       state_next <= state_reg;
+      idx_next <= idx_reg;
+      count_next <= count_reg;
+      render_x_next <= render_x_reg;
+      ready <= '0';
 
       case state_reg is
          when IDLE =>
-            -- state logic
+            ready <= '1';
             if (start = '1') then
-               state_next <= COUNT;
-            end if;
-
-         when COUNT =>
-            -- state logic
-            if (has_char = '1') then
                state_next <= RENDER;
+               idx_next <= 0;
+               render_x_next <= 0;
             end if;
+            for i in 0 to MATH_BLOCK_MAX_CHARS loop
+               if (ascii((i+1)*ASCII_NB-1 downto i*ASCII_NB) /= ASCII_NUL) then
+                  count_next <= i+1;
+               end if;
+            end loop;
 
          when RENDER =>
-            -- state logic
-            if (has_char = '1') then
+            if (idx_reg < count_reg) then
                state_next <= RENDER_CHAR;
             else
                state_next <= IDLE;
             end if;
 
          when RENDER_CHAR =>
-            -- state logic
+            ascii_val := ascii((idx_reg+1)*ASCII_NB-1 downto idx_reg*ASCII_NB);
+            if (ascii_val = ASCII_0) then
+               char_bits(0 to 14) := ascii_0(0 to 14);
+            elsif (ascii_val = ASCII_1) then
+               char_bits(0 to 14) := ascii_1(0 to 14);
+            elsif (ascii_val = ASCII_2) then
+               char_bits(0 to 14) := ascii_2(0 to 14);
+            elsif (ascii_val = ASCII_3) then
+               char_bits(0 to 14) := ascii_3(0 to 14);
+            elsif (ascii_val = ASCII_4) then
+               char_bits(0 to 14) := ascii_4(0 to 14);
+            elsif (ascii_val = ASCII_5) then
+               char_bits(0 to 14) := ascii_5(0 to 14);
+            elsif (ascii_val = ASCII_6) then
+               char_bits(0 to 14) := ascii_6(0 to 14);
+            elsif (ascii_val = ASCII_7) then
+               char_bits(0 to 14) := ascii_7(0 to 14);
+            elsif (ascii_val = ASCII_8) then
+               char_bits(0 to 14) := ascii_8(0 to 14);
+            elsif (ascii_val = ASCII_9) then
+               char_bits(0 to 14) := ascii_9(0 to 14);
+            elsif (ascii_val = ASCII_MUL) then
+               char_bits(0 to 14) := ascii_mul(0 to 14);
+            elsif (ascii_val = ASCII_PLU) then
+               char_bits(0 to 14) := ascii_plu(0 to 14);
+            elsif (ascii_val = ASCII_MIN) then
+               char_bits(0 to 14) := ascii_min(0 to 14);
+            elsif (ascii_val = ASCII_DIV) then
+               char_bits(0 to 14) := ascii_div(0 to 14);
+            elsif (ascii_val = ASCII_EQU) then
+               char_bits(0 to 14) := ascii_equ(0 to 14);
+            end if;
+
+            pixels(render_x_reg+row0 to render_x_reg+row0+2) <= char_bits(0 to 2);
+            pixels(render_x_reg+row1 to render_x_reg+row1+2) <= char_bits(3 to 5);
+            pixels(render_x_reg+row2 to render_x_reg+row2+2) <= char_bits(6 to 8);
+            pixels(render_x_reg+row3 to render_x_reg+row3+2) <= char_bits(9 to 11);
+            pixels(render_x_reg+row4 to render_x_reg+row4+2) <= char_bits(12 to 14);
+
+            idx_next <= idx_reg + 1;
+            render_x_next <= render_x_reg + 4;
             state_next <= RENDER;
 
       end case;
    end process;
+
+   pixels <= pixels_reg;
 end rtl;
