5468424f81663dde31ba30e184e863e135f375b5
rcazzola <rcazzola@comcast.net>
2022-12-10 17:14:50 -0700

    Update to controller to make it work with board

--- vhdl/Controls.vhd
+++ vhdl/Controls.vhd
@@ -30,12 +30,15 @@ use IEEE.NUMERIC_STD.ALL;
 library work;
 use work.DataTypes_pkg.all;
 
+-- Ben Bean
 entity Controls is
    Port (
+      reset, clk:        in std_logic;
       zybo_button_left:  in std_logic;
       zybo_button_right: in std_logic;
       ctrl_buttons:      in std_logic_vector(4 downto 0);
       ctrl_horizontal:   in std_logic_vector(7 downto 0);
+      active_controller: in std_logic;
       x_increment:       out std_logic_vector(ROCKET_MAX_MOVE_RATE_NB downto 0); -- signed, include an extra bit for negatives
       up:                out std_logic;
       down:              out std_logic;
@@ -53,17 +56,30 @@ architecture rtl of Controls is
    signal ctrl_hor_val:   integer range -ROCKET_MAX_MOVE_RATE to ROCKET_MAX_MOVE_RATE;
    signal ctrl_hor_left:  std_logic;
    signal ctrl_hor_right: std_logic;
+   signal x_reg, x_next:  integer range -ROCKET_MAX_MOVE_RATE to ROCKET_MAX_MOVE_RATE;
 begin
 
+   -- Rachel Cazzola 
+   -- state and data register
+   process(clk, reset)
+   begin
+      if (reset = '1') then
+        x_reg <= 0;   
+      elsif (rising_edge(clk)) then
+        x_reg <= x_next;
+      end if;
+   end process;
+
+   -- Ben Bean & Rachel Cazzola
    -- decode controller buttons
-   ctrl_up     <= ctrl_buttons(3);
-   ctrl_down   <= ctrl_buttons(2);
-   ctrl_left   <= ctrl_buttons(1);
-   ctrl_right  <= ctrl_buttons(0);
-   ctrl_joysel <= ctrl_buttons(4);
+   ctrl_up     <= ctrl_buttons(3) AND active_controller;
+   ctrl_down   <= ctrl_buttons(2) AND active_controller;
+   ctrl_left   <= ctrl_buttons(1) AND active_controller;
+   ctrl_right  <= ctrl_buttons(0) AND active_controller;
+   ctrl_joysel <= ctrl_buttons(4) AND active_controller;
 
    -- interpret horizontal values
-   process (ctrl_horizontal)
+   process (ctrl_horizontal, active_controller)
       variable hor_val: signed(ROCKET_MAX_MOVE_RATE_NB downto 0);
       variable hor_ord: std_logic;
    begin
@@ -80,13 +96,21 @@ begin
          end if;
       else
          ctrl_hor_val <= ROCKET_MAX_MOVE_RATE;
-      end if;
+      end if; 
+      -- Rachel Cazzola
+      if (active_controller = '0') then -- switch that controls whether we use the board or controller as our controls
+         ctrl_hor_val <= 0; 
+      end if;    
+      
+       x_next <= ctrl_hor_val;  
    end process;
-
+ 
+   -- Ben Bean & Rachel Cazzola
    x_increment <= std_logic_vector(to_signed(2,x_increment'length))  when (zybo_button_right = '1' or ctrl_right = '1') else
                   std_logic_vector(to_signed(-2,x_increment'length)) when (zybo_button_left = '1'  or ctrl_left = '1')  else
-                  ctrl_horizontal(7 downto 7-ROCKET_MAX_MOVE_RATE_NB);
+                  std_logic_vector(to_unsigned(x_reg, x_increment'length));
 
+   --Ben Bean
    up    <= ctrl_up;
    down  <= ctrl_down;
    left  <= '1' when (ctrl_hor_val < 0) else
